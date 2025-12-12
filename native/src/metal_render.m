76a2ce3798b28c0e22f7098344d7a495386164d1
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-12-12 05:00:55 -0800

    mimappery

--- native/src/metal_render.m
+++ native/src/metal_render.m
@@ -1270,6 +1270,7 @@ AfferentResult afferent_renderer_create(
         MTLSamplerDescriptor *spriteSamplerDesc = [[MTLSamplerDescriptor alloc] init];
         spriteSamplerDesc.minFilter = MTLSamplerMinMagFilterLinear;
         spriteSamplerDesc.magFilter = MTLSamplerMinMagFilterLinear;
+        spriteSamplerDesc.mipFilter = MTLSamplerMipFilterLinear;
         spriteSamplerDesc.sAddressMode = MTLSamplerAddressModeClampToEdge;
         spriteSamplerDesc.tAddressMode = MTLSamplerAddressModeClampToEdge;
         renderer->spriteSampler = [renderer->device newSamplerStateWithDescriptor:spriteSamplerDesc];
@@ -2617,8 +2618,9 @@ static id<MTLTexture> createMetalTexture(id<MTLDevice> device, const uint8_t* da
     MTLTextureDescriptor *desc = [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormatRGBA8Unorm
                                                                                     width:width
                                                                                    height:height
-                                                                                mipmapped:NO];
-    desc.usage = MTLTextureUsageShaderRead;
+                                                                                mipmapped:YES];
+    // Keep this conservative: shader-read is required; render-target helps some drivers/tools with mip generation paths.
+    desc.usage = MTLTextureUsageShaderRead | MTLTextureUsageRenderTarget;
     desc.storageMode = MTLStorageModeManaged;
 
     id<MTLTexture> texture = [device newTextureWithDescriptor:desc];
@@ -2629,6 +2631,68 @@ static id<MTLTexture> createMetalTexture(id<MTLDevice> device, const uint8_t* da
                mipmapLevel:0
                  withBytes:data
                bytesPerRow:width * 4];
+
+    // Generate mip chain on CPU once (avoids needing a blit encoder mid-frame).
+    // This matters a lot when drawing many minified sprites from a large source texture.
+    const uint8_t* prev = data;
+    uint8_t* prevOwned = NULL;
+    uint32_t prevW = width;
+    uint32_t prevH = height;
+
+    uint32_t mipCount = (uint32_t)texture.mipmapLevelCount;
+    for (uint32_t level = 1; level < mipCount; level++) {
+        uint32_t nextW = prevW > 1 ? (prevW / 2) : 1;
+        uint32_t nextH = prevH > 1 ? (prevH / 2) : 1;
+
+        size_t nextSize = (size_t)nextW * (size_t)nextH * 4;
+        uint8_t* next = (uint8_t*)malloc(nextSize);
+        if (!next) {
+            break;
+        }
+
+        for (uint32_t y = 0; y < nextH; y++) {
+            uint32_t sy0 = (2 * y);
+            uint32_t sy1 = (sy0 + 1 < prevH) ? (sy0 + 1) : (prevH - 1);
+            for (uint32_t x = 0; x < nextW; x++) {
+                uint32_t sx0 = (2 * x);
+                uint32_t sx1 = (sx0 + 1 < prevW) ? (sx0 + 1) : (prevW - 1);
+
+                const uint8_t* p00 = prev + ((size_t)sy0 * (size_t)prevW + (size_t)sx0) * 4;
+                const uint8_t* p10 = prev + ((size_t)sy0 * (size_t)prevW + (size_t)sx1) * 4;
+                const uint8_t* p01 = prev + ((size_t)sy1 * (size_t)prevW + (size_t)sx0) * 4;
+                const uint8_t* p11 = prev + ((size_t)sy1 * (size_t)prevW + (size_t)sx1) * 4;
+
+                uint32_t r = (uint32_t)p00[0] + (uint32_t)p10[0] + (uint32_t)p01[0] + (uint32_t)p11[0];
+                uint32_t g = (uint32_t)p00[1] + (uint32_t)p10[1] + (uint32_t)p01[1] + (uint32_t)p11[1];
+                uint32_t b = (uint32_t)p00[2] + (uint32_t)p10[2] + (uint32_t)p01[2] + (uint32_t)p11[2];
+                uint32_t a = (uint32_t)p00[3] + (uint32_t)p10[3] + (uint32_t)p01[3] + (uint32_t)p11[3];
+
+                uint8_t* dst = next + ((size_t)y * (size_t)nextW + (size_t)x) * 4;
+                dst[0] = (uint8_t)(r >> 2);
+                dst[1] = (uint8_t)(g >> 2);
+                dst[2] = (uint8_t)(b >> 2);
+                dst[3] = (uint8_t)(a >> 2);
+            }
+        }
+
+        MTLRegion mipRegion = MTLRegionMake2D(0, 0, nextW, nextH);
+        [texture replaceRegion:mipRegion
+                   mipmapLevel:level
+                     withBytes:next
+                   bytesPerRow:nextW * 4];
+
+        if (prevOwned) {
+            free(prevOwned);
+        }
+        prev = next;
+        prevOwned = next;
+        prevW = nextW;
+        prevH = nextH;
+    }
+
+    if (prevOwned) {
+        free(prevOwned);
+    }
 
     return texture;
 }
