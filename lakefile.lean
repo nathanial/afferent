2faa59bcd9864905ea3547066d9d5703f9e0a055
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-12-28 22:23:04 -0800

    Switch to GitHub dependencies for v0.0.1 release

--- lakefile.lean
+++ lakefile.lean
@@ -5,14 +5,14 @@ open System (FilePath)
 package afferent where
   version := v!"0.1.0"
 
-require collimator from ".." / "collimator"
-require crucible from ".." / "crucible"
-require tincture from ".." / "tincture"
-require trellis from ".." / "trellis"
-require arbor from ".." / "arbor"
-require assimptor from ".." / "assimptor"
-require staple from ".." / "staple"
-require linalg from ".." / "linalg"
+require collimator from git "https://github.com/nathanial/collimator" @ "v0.0.1"
+require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.1"
+require tincture from git "https://github.com/nathanial/tincture" @ "v0.0.1"
+require trellis from git "https://github.com/nathanial/trellis" @ "v0.0.1"
+require arbor from git "https://github.com/nathanial/arbor" @ "v0.0.1"
+require assimptor from git "https://github.com/nathanial/assimptor" @ "v0.0.1"
+require staple from git "https://github.com/nathanial/staple" @ "v0.0.1"
+require linalg from git "https://github.com/nathanial/linalg" @ "v0.0.1"
 
 -- Common link arguments for all executables
 -- Includes both Homebrew paths for Apple Silicon (/opt/homebrew) and Intel (/usr/local)
