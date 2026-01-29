f6a876812b2b3de1e91a215f687b3590a575bb8f
Nathanial Hartman <nathanial.hartman@gmail.com>
2026-01-28 18:40:20 -0800

    updated version

--- lakefile.lean
+++ lakefile.lean
@@ -11,7 +11,7 @@ require trellis from git "https://github.com/nathanial/trellis" @ "v0.0.14"
 require assimptor from git "https://github.com/nathanial/assimptor" @ "v0.0.2"
 require staple from git "https://github.com/nathanial/staple" @ "v0.0.2"
 require linalg from git "https://github.com/nathanial/linalg" @ "v0.0.3"
-require reactive from git "https://github.com/nathanial/reactive" @ "v0.2.1"
+require reactive from git "https://github.com/nathanial/reactive" @ "v0.2.2"
 require raster from git "https://github.com/nathanial/raster" @ "v0.0.5"
 require oracle from git "https://github.com/nathanial/oracle" @ "v0.2.0"
 
