472dd6232f0f17345834b96b598b08a90e2175ee
Nathanial Hartman <nathanial.hartman@gmail.com>
2026-02-04 17:34:56 -0800

    extract progress bar

--- Afferent/Canopy/Widget/Display.lean
+++ Afferent/Canopy/Widget/Display.lean
@@ -7,7 +7,6 @@ import Afferent.Canopy.Widget.Display.Badge
 import Afferent.Canopy.Widget.Display.Chip
 import Afferent.Canopy.Widget.Display.Label
 import Afferent.Canopy.Widget.Display.Link
-import Afferent.Canopy.Widget.Display.ProgressBar
 import Afferent.Canopy.Widget.Display.Separator
 import Afferent.Canopy.Widget.Display.Toast
 import Afferent.Canopy.Widget.Display.Tooltip
